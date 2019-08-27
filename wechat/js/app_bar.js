6d2e10df892df8cef364043edf0be0670bb0cba3
mengyuxiang0820 <18434077505@163.com>
2019-08-27 17:25:18 +0800

    微信

---
+++ wechat/js/app_bar.js
@@ -0,0 +1,28 @@
+$(function() {
+  //每1秒 执行的函数  // 定时任务
+  var task = setInterval("updateQuantity()", 1000)
+  $("#batteray").click(function() {
+    clearInterval(task);
+  })
+})
+
+function updateQuantity() {
+
+  if($("#batteray_body").width() + 1 > $("#batteray").width()) {
+    $("#batteray_body").width(1);
+  } else {
+    $("#batteray_body").width( $("#batteray_body").width() + 1 )
+  }
+
+  var curr = $("#batteray_body").width() / $("#batteray").width()
+
+  if(curr <= 0.2) {
+    $("#batteray_body").css('background-color', '#f44336');
+  } else if(curr > 0.2 && curr <= 0.8) {
+    $("#batteray_body").css('background-color', '#FFF176');
+  } else if(curr > 0.8) {
+    $("#batteray_body").css('background-color', '#d4d4d4');
+  } else {
+    console.log("batteray  boom !!!!!!!  peng !!!!!")
+  }
+}
---
+++ wechat/js/title.js
@@ -0,0 +1,14 @@
+$(function () {
+   $('title_submenu') .hide();
+   $('#add').click(function () {
+       $('#title_submenu').toggle(200)
+       e.stopPropagation()
+   })
+})
+$(function () {
+    $('title_submenu') .hide();
+    $('#camera').click(function () {
+        $('#title_submenu').toggle(200)
+        e.stopPropagation()
+    })
+})
